2ffe89cb0f09ac31d3d0ddf3fd9bb08d3809d0e8
Nuttanun Siriaporn <33064200+nsiriaporn@users.noreply.github.com>
2024-01-29 23:16:19 +0100

    Update play-store.js
    Update to new json2csv and Node.js format

--- play-store.js
+++ play-store.js
@@ -1,8 +1,8 @@
 //search google play store for mobile apps that supports active transport
 
-scraper = require('google-play-scraper'); // Import play store scraper 
-converter = require('json-2-csv');
-fs = require('fs'); //Import the module to use files
+const { default: scraper } = await import("google-play-scraper"); // Import play store scraper 
+const { default: converter } = await import("json-2-csv");
+const { default: fs } = await import("fs"); //Import the module to use files
 
 //Function to translate the result of the request in CSV
 //and write it into the file resultScrapingRequest.txt
@@ -45,14 +45,10 @@ csv2file = function(msg){
         delete element['recentChanges'];
         delete element['editorsChoice']
     });
-    converter.json2csv(msg,(err,csv)=> {
-        if(err){
-            throw err;
-        }
-        fs.appendFile("resultScrapingRequest.txt", csv, function(err){
-            if(err){ return truelog(err);}
-        });
-    });
+    csv = converter.json2csv(msg)
+    fs.appendFile("resultScrapingRequest.txt", csv, (err)=>{
+            if(err){ console.log("Error in appendFile."); return truelog(err);}
+     	});
     console.log("Request fullfield.");
 };
 
